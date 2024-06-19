7136f63f844da4d19adc83ae230bd70bf87ad565
randomme123 <destombesl@gmail.com>
2024-06-20 00:24:02 +0200

    Automated commit after build and run - 2024-06-20 00:24:02

--- program.ps1
+++ program.ps1
@@ -1,16 +1,32 @@
-# Run the build script
+# build exe
+Write-Host ""
+Write-Host "Build .exe."
+
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\build.ps1"
+
+Write-Host ""
+Write-Host "Run .exe."
 
 # Run the application
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\run.ps1"
 
+Write-Host ""
+Write-Host "Clean Up."
+
 # Clean up the build directory
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\clean.ps1"
+
+Write-Host ""
+Write-Host "Start SSH Service."
 
 # start ssh service
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\sshagent.ps1"
 
+Write-Host ""
+Write-Host "Commit to Github."
+
 # Commit everything to github
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\commit.ps1"
 
-Write-Host "All tasks completed."
+Write-Host ""
+Write-Host "Program Completed."
