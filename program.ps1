a8b3fd749a5e9537ebe98ee90b3db38e3130d0da
randomme123 <destombesl@gmail.com>
2024-06-20 10:20:32 +0200

    Automated commit after build and run - 2024-06-20 10:20:32

--- program.ps1
+++ program.ps1
@@ -1,59 +1,59 @@
-# Build exe
 Write-Host ""
 Write-Host ">>> >>> Build Executable <<< <<<"
 Write-Host ""
-$buildTime = Measure-Command {
-    & "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Build.ps1"
-}
+
+$startTime = Get-Date
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Build.ps1"
+$endTime = Get-Date
+$buildTime = $endTime - $startTime
+
 Write-Host "Time taken for Build .exe: $($buildTime.TotalSeconds) seconds"
 Write-Host ""
 
-
-# Run exe
-Write-Host ""
 Write-Host ">>> >>> Run Executable <<< <<<"
 Write-Host ""
-$runTime = Measure-Command {
-    & "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Run.ps1"
-}
+
+$startTime = Get-Date
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Run.ps1"
+$endTime = Get-Date
+$runTime = $endTime - $startTime
+
 Write-Host "Time taken for Run .exe: $($runTime.TotalSeconds) seconds"
 Write-Host ""
 
-
-# Dependencies
-Write-Host ""
 Write-Host ">>> >>> Deps <<< <<<"
 Write-Host ""
-$depsTime = Measure-Command {
-    & "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Deps.ps1"
-}
+
+$startTime = Get-Date
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Deps.ps1"
+$endTime = Get-Date
+$depsTime = $endTime - $startTime
+
 Write-Host "Time taken for Deps: $($depsTime.TotalSeconds) seconds"
 Write-Host ""
 
-
-# Cleanup
-Write-Host ""
 Write-Host ">>> >>> Clean Up <<< <<<"
 Write-Host ""
-$cleanTime = Measure-Command {
-    & "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Clean.ps1"
-}
+
+$startTime = Get-Date
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Clean.ps1"
+$endTime = Get-Date
+$cleanTime = $endTime - $startTime
+
 Write-Host "Time taken for Clean Up: $($cleanTime.TotalSeconds) seconds"
 Write-Host ""
 
-
-# Commit to github
-Write-Host ""
 Write-Host ">>> >>> Commit to GitHub <<< <<<"
 Write-Host ""
-$commitTime = Measure-Command {
-    & "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Commit.ps1"
-}
+
+$startTime = Get-Date
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\Commit.ps1"
+$endTime = Get-Date
+$commitTime = $endTime - $startTime
+
 Write-Host "Time taken for Commit to GitHub: $($commitTime.TotalSeconds) seconds"
 Write-Host ""
 
-
-# finish
-Write-Host ""
 Write-Host ">>> >>> Program Completed <<< <<<"
 Write-Host ""
+
