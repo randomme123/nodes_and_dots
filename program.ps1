3755328bb90f298e85b314bb9a8df45823884bda
randomme123 <destombesl@gmail.com>
2024-06-20 04:41:42 +0200

    Automated commit after build and run - 2024-06-20 04:41:42

--- program.ps1
+++ program.ps1
@@ -1,23 +1,28 @@
 # Build exe
 Write-Host ""
-Write-Host "Build .exe."
+Write-Host ">>> >>> Build .exe <<< <<<"
+Write-Host ""
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\build.ps1"
 
 # Run exe
 Write-Host ""
-Write-Host "Run .exe."
+Write-Host ">>> >>> Run .exe <<< <<<"
+Write-Host ""
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\run.ps1"
 
 # Clean up the build dir and docs
 Write-Host ""
-Write-Host "Clean Up."
+Write-Host ">>> >>> Clean Up <<< <<<"
+Write-Host ""
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\clean.ps1"
 
 # Commit to GitHub
 Write-Host ""
-Write-Host "Commit to GitHub."
+Write-Host ">>> >>> Commit to GitHub <<< <<<"
+Write-Host ""
 & "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\commit.ps1"
 
 # finish up
 Write-Host ""
-Write-Host "Program Completed."
+Write-Host ">>> >>> Program Completed <<< <<<"
+Write-Host ""
