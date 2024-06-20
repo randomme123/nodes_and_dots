93c336cd51332f97e52a577a055fbc8560eb194f
randomme123 <destombesl@gmail.com>
2024-06-20 05:06:38 +0200

    Automated commit after build and run - 2024-06-20 05:06:38

--- program.ps1
+++ program.ps1
@@ -2,32 +2,32 @@
 Write-Host ""
 Write-Host ">>> >>> Build .exe <<< <<<"
 Write-Host ""
-& "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\build.ps1"
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\build.ps1"
 
 # Run exe
 Write-Host ""
 Write-Host ">>> >>> Run .exe <<< <<<"
 Write-Host ""
-& "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\run.ps1"
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\run.ps1"
 
 # deps
 Write-Host ""
 Write-Host ">>> >>> Deps <<< <<<"
 Write-Host ""
-& "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\deps.ps1"
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\deps.ps1"
 
 
 # Clean up the build dir and docs
 Write-Host ""
 Write-Host ">>> >>> Clean Up <<< <<<"
 Write-Host ""
-& "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\clean.ps1"
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\clean.ps1"
 
 # Commit to GitHub
 Write-Host ""
 Write-Host ">>> >>> Commit to GitHub <<< <<<"
 Write-Host ""
-& "C:\Users\desto\Scripts\projects\nodes_and_dots\powershell\commit.ps1"
+& "C:\Users\desto\Scripts\projects\nodes_and_dots\shell\commit.ps1"
 
 # finish up
 Write-Host ""
